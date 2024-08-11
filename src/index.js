58c7caad06eaa6701b8fb13a3e181c73d7fc85da
unknown <lena.kilian91@gmail.com>
2024-08-12 01:34:47 +0800

    adding solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const dividedToWords = expr.split('**********');
+    console.log(dividedToWords)
+    const getDecodedWord = dividedToWords.map((word) => {
+      console.log(word)
+        let letterArray = [];
+        for (let i = 0; i < word.length; i += 10) {
+            const letter = word.slice(i, i + 10);
+            let codedLetter = '';
+            for (let j=0; j < letter.length; j +=2) {
+                if ((letter[j] + letter[j+1]) === '10') {
+                    codedLetter += '.';
+                }
+                else if ((letter[j] + letter[j+1]) === '11') {
+                    codedLetter += '-'
+                }
+                else if ((letter[j] + letter[j+1]) === '00') {
+                    codedLetter += ''
+                }
+                console.log(codedLetter)
+            }
+            letterArray.push(MORSE_TABLE[codedLetter]);
+            
+        }
+        return letterArray.join('');
+    });
+    return getDecodedWord.join(' ');
 }
-
 module.exports = {
     decode
 }
